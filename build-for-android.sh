0c48c49fa7dfcf2b0c117633ab1d510ed821d376
hATrayflood <h.rayflood@gmail.com>
2019-02-07 13:22:41 +0900

    improvement for android on linux.

--- build-for-android.sh
+++ build-for-android.sh
@@ -4,6 +4,11 @@ yarn build
 rm build/_headers
 rm build/asset-manifest.json
 rm build/manifest.json
+rm build/precache-manifest.*.js
+rm build/service-worker.js
+rm build/favicon.png
+rm build/unsupported.html
 
 rm build/static/css/*.map
 rm build/static/js/*.map
+rm build/static/js/runtime~main.js
