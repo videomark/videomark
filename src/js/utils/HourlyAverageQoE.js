25f3ace17d02bbb1f632a0c2081a8e403ba0b589
Kohei Watanabe <kou029w@gmail.com>
2019-04-16 18:44:19 +0900

    HourlyAverageQoEクラス実装

---
+++ src/js/utils/HourlyAverageQoE.js
@@ -0,0 +1,28 @@
+import Api from "./Api";
+
+class HourlyAverageQoE {
+  async fetchHourlyQoEAPI() {
+    if (!window.navigator.onLine) return undefined;
+    const response = await Api.hour();
+    if (!response.ok) return undefined;
+    const json = await response.json();
+
+    if (!Array.isArray(json)) return undefined;
+    this.cache = json.reduce(
+      (a, { hour, average }) => Object.assign(a, { [hour]: average }),
+      {}
+    );
+    return this.cache;
+  }
+
+  async init() {
+    await this.fetchHourlyQoEAPI();
+    return this.cache;
+  }
+
+  async at(hour) {
+    if (this.cache[hour] === undefined) await this.fetchHourlyQoEAPI();
+    return this.cache[hour];
+  }
+}
+export default HourlyAverageQoE;
