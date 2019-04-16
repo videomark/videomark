9d2376dc21cee83c5318a1f53ae728aa47a7e425
Kohei Watanabe <kou029w@gmail.com>
2019-04-16 14:47:44 +0900

    AppDataを介してフィルターする

--- src/js/containers/ViewingList.jsx
+++ src/js/containers/ViewingList.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Viewing from "./Viewing";
 import ChromeExtensionWrapper from "../utils/ChromeExtensionWrapper";
+import AppData from "../utils/AppData";
+import AppDataActions from "../utils/AppDataActions";
+import { Services, LocationToService } from "../utils/Utils";
 
 class ViewingList extends Component {
   constructor() {
     super();
-    this.state = { viewings: [] };
+    this.state = {
+      viewings: [],
+      sites: Object.values(Services),
+      date: new Date()
+    };
   }
 
   async componentDidMount() {
@@ -13,19 +20,31 @@ class ViewingList extends Component {
       ChromeExtensionWrapper.loadVideoIds(resolve);
     });
     this.setState({
-      viewings: viewings.map(({ id, data }) => ({
-        key: id,
-        sessionId: data.session_id,
-        videoId: data.video_id
-      }))
+      viewings: viewings
+        .map(({ id, data }) => ({
+          key: id,
+          sessionId: data.session_id,
+          videoId: data.video_id,
+          location: data.location,
+          startTime: new Date(data.start_time)
+        }))
+        .sort((a, b) => a.startTime - b.startTime)
     });
+    AppData.add(AppDataActions.ViewingList, this, "setState");
   }
 
   render() {
-    const { viewings } = this.state;
-    return viewings.map(({ key, sessionId, videoId }) => (
-      <Viewing key={key} sessionId={sessionId} videoId={videoId} />
-    ));
+    const { viewings, sites, date } = this.state;
+    return viewings
+      .filter(({ location }) => sites.includes(LocationToService(location)))
+      .filter(
+        ({ startTime }) =>
+          startTime.getFullYear() === date.getFullYear() &&
+          startTime.getMonth() === date.getMonth()
+      )
+      .map(({ key, sessionId, videoId }) => (
+        <Viewing key={key} sessionId={sessionId} videoId={videoId} />
+      ));
   }
 }
 export default ViewingList;
--- src/js/utils/AppDataActions.js
+++ src/js/utils/AppDataActions.js
@@ -1,5 +1,6 @@
 export default {
   SiteFilter: "SiteFilter",
   MonthFilter: "MonthFilter",
-  Modal: "Modal"
+  Modal: "Modal",
+  ViewingList: "ViewingList"
 };
