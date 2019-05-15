9f5dd6b6df387264e4a78f873931d84eec061929
Kohei Watanabe <kou029w@gmail.com>
2019-05-15 16:31:49 +0900

    品質を表示する部分をQualityコンポーネントとして切り出す

---
+++ src/js/modules/UI/Quality.js
@@ -0,0 +1,49 @@
+import { html } from "lit-html";
+import Storage from "../Storage";
+
+const state = {};
+export const useStorage = ({ sessionId, videoId }) => {
+  const id = `${sessionId}_${videoId}`;
+  if (!(state[id] instanceof Storage)) {
+    state[id] = new Storage({ sessionId, videoId });
+    state[id].init();
+  }
+  return state[id];
+};
+
+const latest = (log, key) =>
+  ((log || []).filter(a => key in a).slice(-1)[0] || {})[key];
+
+export const latestQoE = ({ sessionId, videoId }) => {
+  const storage = useStorage({ sessionId, videoId });
+  if (storage.cache === undefined) return NaN;
+  const qoe = latest(storage.cache.log, "qoe");
+  return qoe == null ? NaN : qoe;
+};
+
+export const latestQuolity = ({ sessionId, videoId }) => {
+  const storage = useStorage({ sessionId, videoId });
+  if (storage.cache === undefined) return "";
+  const quolity = latest(storage.cache.log, "quolity");
+  return quolity || {};
+};
+
+export const quality = ({ sessionId, videoId }) => {
+  const quolity = latestQuolity({ sessionId, videoId });
+  const { droppedVideoFrames, totalVideoFrames } = quolity;
+  if ([droppedVideoFrames, totalVideoFrames].some(n => !Number.isFinite(n)))
+    return "";
+  const qoe = latestQoE({ sessionId, videoId });
+
+  return html`
+    <dl>
+      <dt>フレームドロップ率</dt>
+      <dd>
+        ${((droppedVideoFrames / totalVideoFrames) * 100).toFixed(2)} % (
+        ${droppedVideoFrames} / ${totalVideoFrames} )
+      </dd>
+      <dt>体感品質 (QoE)</dt>
+      <dd>${Number.isFinite(qoe) ? qoe : "..."}</dd>
+    </dl>
+  `;
+};
--- src/js/modules/UI/Status.js
+++ src/js/modules/UI/Status.js
@@ -1,47 +1,5 @@
 import { html, render } from "lit-html";
-import Storage from "../Storage";
-
-const state = {};
-const useStorage = ({ sessionId, videoId }) => {
-  const id = `${sessionId}_${videoId}`;
-  if (!(state[id] instanceof Storage)) {
-    state[id] = new Storage({ sessionId, videoId });
-    state[id].init();
-  }
-  return state[id];
-};
-
-const latest = (log, key) =>
-  ((log || []).filter(a => key in a).slice(-1)[0] || {})[key];
-
-const latestQoE = ({ sessionId, videoId }) => {
-  const storage = useStorage({ sessionId, videoId });
-  if (storage.cache === undefined) return NaN;
-  const qoe = latest(storage.cache.log, "qoe");
-  return qoe == null ? NaN : qoe;
-};
-
-const details = ({ sessionId, videoId }) => {
-  const storage = useStorage({ sessionId, videoId });
-  if (storage.cache === undefined) return "";
-  const quolity = latest(storage.cache.log, "quolity") || {};
-  const { droppedVideoFrames, totalVideoFrames } = quolity;
-  if ([droppedVideoFrames, totalVideoFrames].some(n => !Number.isFinite(n)))
-    return "";
-  const qoe = latestQoE({ sessionId, videoId });
-
-  return html`
-    <dl>
-      <dt>フレームドロップ率</dt>
-      <dd>
-        ${((droppedVideoFrames / totalVideoFrames) * 100).toFixed(2)} % (
-        ${droppedVideoFrames} / ${totalVideoFrames} )
-      </dd>
-      <dt>体感品質 (QoE)</dt>
-      <dd>${Number.isFinite(qoe) ? qoe : "..."}</dd>
-    </dl>
-  `;
-};
+import { quality, latestQoE } from "./Quality";
 
 export default class Status {
   constructor() {
@@ -86,7 +44,7 @@ export default class Status {
           <summary>
             ${Number.isFinite(qoe) ? `QoE: ${qoe.toFixed(2)}` : "計測中..."}
           </summary>
-          ${open ? details({ sessionId, videoId }) : ""}
+          ${open ? quality({ sessionId, videoId }) : ""}
         </details>
       </div>
     `;
