974cec594b98d10d2993e1a715e2dfc3a7cbe762
Kohei Watanabe <kou029w@gmail.com>
2019-05-20 12:12:36 +0900

    フレームドロップが発生したことを警告する

--- src/js/components/VideoQuality.jsx
+++ src/js/components/VideoQuality.jsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { Grid, Typography } from "@material-ui/core";
+import { Warning } from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = () => ({
+  typography: {
+    useNextVariants: true
+  },
+  warningIcon: {
+    fontSize: 20,
+    verticalAlign: "bottom"
+  }
+});
 
 const VideoQuality = ({
+  classes,
   framerate: fps,
   droppedVideoFrames: dropped,
   totalVideoFrames: total
 }) => {
   if ([fps, dropped / total].some(n => !Number.isFinite(n))) return null;
+  const isLowQuality = dropped / total > 1e-3;
 
   return (
     <Grid container>
@@ -19,15 +33,28 @@ const VideoQuality = ({
       </Grid>
       <Grid item xs>
         <Typography component="dt">フレームドロップ率</Typography>
-        <Typography component="dd">
+        <Typography
+          component="dd"
+          gutterBottom={isLowQuality}
+          color={isLowQuality ? "error" : "default"}
+        >
           {`${((dropped / total) * 100).toFixed(2)} % (${dropped} / ${total})`}
         </Typography>
       </Grid>
+      {isLowQuality ? (
+        <Grid item xs={12}>
+          <Typography variant="caption" component="small" color="error">
+            <Warning className={classes.warningIcon} />
+            フレームドロップが発生したため実際の体感品質とは異なる可能性があります。
+          </Typography>
+        </Grid>
+      ) : null}
     </Grid>
   );
 };
 
 VideoQuality.propTypes = {
+  classes: PropTypes.instanceOf(Object).isRequired,
   framerate: PropTypes.number,
   droppedVideoFrames: PropTypes.number,
   totalVideoFrames: PropTypes.number
@@ -38,4 +65,4 @@ VideoQuality.defaultProps = {
   totalVideoFrames: undefined
 };
 
-export default VideoQuality;
+export default withStyles(styles)(VideoQuality);
