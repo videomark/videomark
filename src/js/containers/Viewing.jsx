40fe9ddbf72cde4b4186aeec9a4b7d2d0f449ab7
Kohei Watanabe <kou029w@gmail.com>
2019-06-11 16:00:02 +0900

    一部Functional Component化

--- src/js/containers/Viewing.jsx
+++ src/js/containers/Viewing.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
@@ -17,168 +17,184 @@ import AppData from "../utils/AppData";
 import AppDataActions from "../utils/AppDataActions";
 import NoImage from "../../images/noimage.svg";
 
-const toTimeString = date => {
+export const VideoThumbnail = ({ title, thumbnail, disabled }) => (
+  <img
+    className={style.thumbnail + (disabled ? ` ${style.removedThumbnail}` : "")}
+    src={thumbnail}
+    alt={title}
+    onError={e => {
+      e.target.src = NoImage;
+    }}
+  />
+);
+VideoThumbnail.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  disabled: PropTypes.bool
+};
+VideoThumbnail.defaultProps = {
+  disabled: false
+};
+
+export const toTimeString = date => {
   return `${date.getFullYear()}/${date.getMonth() +
     1}/${`0${date.getDate()}`.slice(
     -2
   )} ${date.getHours()}:${`0${date.getMinutes()}`.slice(-2)}`;
 };
 
-class Viewing extends Component {
-  constructor(props) {
-    super(props);
-    const { sessionId, videoId, disabled } = props;
-    this.viewing = new ViewingModel({ sessionId, videoId });
-    this.state = {
-      title: "",
-      thumbnail: "",
-      startTime: new Date(),
-      qoe: 0,
-      disabled
-    };
-  }
+export const fetch = async ({
+  sessionId,
+  videoId,
+  regionalStats,
+  hourlyStats
+}) => {
+  const viewing = new ViewingModel({ sessionId, videoId });
+  const id = await viewing.init();
+  const title = await viewing.title;
+  const location = await viewing.location;
+  const thumbnail = await viewing.thumbnail;
+  const startTime = await viewing.startTime;
+  const qoe = await viewing.qoe;
+  const quality = await viewing.quality;
+  const region = (await viewing.region) || {};
+  const regionalAverage = await regionalStats.at(region);
+  const hourlyAverage = await hourlyStats.at(startTime.getHours());
+  return {
+    id,
+    title,
+    location,
+    thumbnail,
+    startTime,
+    qoe,
+    quality,
+    region,
+    regionalAverage,
+    hourlyAverage
+  };
+};
 
-  async componentDidMount() {
-    const { viewing } = this;
-    const id = await viewing.init();
-    this.setState({
-      id,
-      title: await viewing.title,
-      location: await viewing.location,
-      thumbnail: await viewing.thumbnail,
-      startTime: await viewing.startTime,
-      qoe: await viewing.qoe
-    });
-    const { regionalAverageQoE } = this.props;
-    const region = (await viewing.region) || {};
-    this.setState({
-      region,
-      regionalAverageQoE: await regionalAverageQoE.at(region)
-    });
-    const { hourlyAverageQoE } = this.props;
-    const startTime = await viewing.startTime;
-    const hour = startTime.getHours();
-    this.setState({
-      hour,
-      hourlyAverageQoE: await hourlyAverageQoE.at(hour)
-    });
-  }
+const Viewing = ({
+  sessionId,
+  videoId,
+  regionalAverageQoE: regionalStats,
+  hourlyAverageQoE: hourlyStats,
+  disabled
+}) => {
+  const [viewing, setViewing] = useState();
+  useEffect(() => {
+    (async () => {
+      setViewing(
+        await fetch({ sessionId, videoId, regionalStats, hourlyStats })
+      );
+    })();
+  }, [setViewing]);
+  if (!viewing) return null;
+  const {
+    id,
+    title,
+    location,
+    thumbnail,
+    startTime,
+    qoe,
+    region,
+    regionalAverage,
+    hourlyAverage
+  } = viewing;
 
-  render() {
-    const {
-      id,
-      title,
-      location,
-      thumbnail,
-      startTime,
-      qoe,
-      region,
-      regionalAverageQoE,
-      hour,
-      hourlyAverageQoE,
-      disabled
-    } = this.state;
-
-    const VideoThumbnail = () => (
-      <img
-        className={
-          style.thumbnail + (disabled ? ` ${style.removedThumbnail}` : "")
-        }
-        src={thumbnail}
-        alt={title}
-        onError={e => {
-          e.target.src = NoImage;
+  const GraphList = () => (
+    <Grid container style={{ height: 76 }} alignItems="center">
+      <Grid item xs style={{ paddingLeft: 4, paddingRight: 4 }}>
+        <QoEValueGraphList
+          value={qoe}
+          region={region}
+          regionalAverage={regionalAverage}
+          hour={startTime.getHours()}
+          hourlyAverage={hourlyAverage}
+        />
+      </Grid>
+    </Grid>
+  );
+  const RecoverOrRemoveButton = () => (
+    <div className={style.removedStateButtons}>
+      <Button
+        variant="contained"
+        color="primary"
+        className={style.removedStateButton}
+        onClick={() => {
+          DataErase.recover(id);
+          // FIXME: ViewingListをrender()しないと表示が変わらない
+          AppData.update(AppDataActions.ViewingList, state => state);
         }}
-      />
-    );
-
-    const GraphList = () => (
-      <Grid container style={{ height: 76 }} alignItems="center">
-        <Grid item xs style={{ paddingLeft: 4, paddingRight: 4 }}>
-          <QoEValueGraphList
-            value={qoe}
-            region={region}
-            regionalAverage={regionalAverageQoE}
-            hour={hour}
-            hourlyAverage={hourlyAverageQoE}
-          />
-        </Grid>
-      </Grid>
-    );
-
-    const RecoverOrRemoveButton = () => (
-      <div className={style.removedStateButtons}>
-        <Button
-          variant="contained"
-          color="primary"
-          className={style.removedStateButton}
-          onClick={() => {
-            DataErase.recover(id);
-            // FIXME: ViewingListをrender()しないと表示が変わらない
-            AppData.update(AppDataActions.ViewingList, state => state);
-          }}
-        >
-          <Refresh />
-          <span>&nbsp;復元</span>
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          className={style.removedStateButton}
-          onClick={async () => {
-            await DataErase.remove(id);
-            AppData.update(AppDataActions.ViewingList, state =>
-              Object.assign(state, {
-                viewings: state.viewings.filter(viewing => viewing.id !== id)
-              })
-            );
-          }}
-        >
-          <CrossIcon />
-          <span>&nbsp;削除</span>
-        </Button>
-      </div>
-    );
-    const DisabledInfo = () => (
-      <Grid
-        container
-        style={{ height: 76 }}
-        direction="column"
-        justify="flex-end"
       >
-        <Grid item>
-          <Typography
-            component="small"
-            variant="caption"
-            color="error"
-            align="center"
-            display="block"
-            gutterBottom
-            style={{ lineHeight: 1.2 }}
-          >
-            次回起動時に削除します。取り消すには復元、今すぐ削除するには削除を押してください。
-          </Typography>
-          <RecoverOrRemoveButton />
-        </Grid>
+        <Refresh />
+        <span>&nbsp;復元</span>
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={style.removedStateButton}
+        onClick={async () => {
+          await DataErase.remove(id);
+          AppData.update(AppDataActions.ViewingList, state =>
+            Object.assign(state, {
+              viewings: state.viewings.filter(v => v.id !== id)
+            })
+          );
+        }}
+      >
+        <CrossIcon />
+        <span>&nbsp;削除</span>
+      </Button>
+    </div>
+  );
+  const DisabledInfo = () => (
+    <Grid
+      container
+      style={{ height: 76 }}
+      direction="column"
+      justify="flex-end"
+    >
+      <Grid item>
+        <Typography
+          component="small"
+          variant="caption"
+          color="error"
+          align="center"
+          display="block"
+          gutterBottom
+          style={{ lineHeight: 1.2 }}
+        >
+          次回起動時に削除します。取り消すには復元、今すぐ削除するには削除を押してください。
+        </Typography>
+        <RecoverOrRemoveButton />
       </Grid>
-    );
+    </Grid>
+  );
 
-    return (
-      <Card>
-        <div className={style.header}>
-          <CardMedia component={VideoThumbnail} image="#" />
-          <div className={style.movieInfo}>
-            <span>{urlToVideoPlatform(location).name}</span>
-            <span>{toTimeString(startTime)}</span>
-          </div>
+  return (
+    <Card>
+      <div className={style.header}>
+        <CardMedia
+          component={() => (
+            <VideoThumbnail
+              title={title}
+              thumbnail={thumbnail}
+              disabled={disabled}
+            />
+          )}
+          image="#"
+        />
+        <div className={style.movieInfo}>
+          <span>{urlToVideoPlatform(location).name}</span>
+          <span>{toTimeString(startTime)}</span>
         </div>
-        <div className={style.title}>{title}</div>
-        {disabled ? <DisabledInfo /> : <GraphList />}
-      </Card>
-    );
-  }
-}
-
+      </div>
+      <div className={style.title}>{title}</div>
+      {disabled ? <DisabledInfo /> : <GraphList />}
+    </Card>
+  );
+};
 Viewing.propTypes = {
   sessionId: PropTypes.string.isRequired,
   videoId: PropTypes.string.isRequired,
@@ -186,11 +202,9 @@ Viewing.propTypes = {
   hourlyAverageQoE: PropTypes.instanceOf(HourlyAverageQoE),
   disabled: PropTypes.bool
 };
-
 Viewing.defaultProps = {
   regionalAverageQoE: new RegionalAverageQoE(),
   hourlyAverageQoE: new HourlyAverageQoE(),
   disabled: false
 };
-
-export default Viewing;
+export default React.memo(Viewing);
--- src/js/containers/ViewingDetail.jsx
+++ src/js/containers/ViewingDetail.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
@@ -10,126 +10,87 @@ import AppDataActions from "../utils/AppDataActions";
 import AppData from "../utils/AppData";
 import { urlToVideoPlatform } from "../utils/Utils";
 import style from "../../css/MeasureContents.module.css";
-import ViewingModel from "../utils/Viewing";
 import RegionalAverageQoE from "../utils/RegionalAverageQoE";
 import HourlyAverageQoE from "../utils/HourlyAverageQoE";
-import NoImage from "../../images/noimage.svg";
+import { VideoThumbnail, toTimeString, fetch } from "./Viewing";
 
-const toTimeString = date => {
-  return `${date.getFullYear()}/${date.getMonth() +
-    1}/${`0${date.getDate()}`.slice(
-    -2
-  )} ${date.getHours()}:${`0${date.getMinutes()}`.slice(-2)}`;
-};
+const ViewingDetail = ({
+  sessionId,
+  videoId,
+  regionalAverageQoE: regionalStats,
+  hourlyAverageQoE: hourlyStats
+}) => {
+  const [viewing, setViewing] = useState();
+  useEffect(() => {
+    if (viewing) return;
+    (async () => {
+      setViewing(
+        await fetch({ sessionId, videoId, regionalStats, hourlyStats })
+      );
+    })();
+  }, [setViewing]);
+  if (!viewing) return null;
+  const {
+    id,
+    title,
+    location,
+    thumbnail,
+    startTime,
+    qoe,
+    quality,
+    region,
+    regionalAverage,
+    hourlyAverage
+  } = viewing;
 
-class ViewingDetail extends Component {
-  constructor(props) {
-    super(props);
-    const { sessionId, videoId } = props;
-    this.viewing = new ViewingModel({ sessionId, videoId });
-    this.state = {
-      id: this.viewing.viewingId,
-      title: "",
-      thumbnail: "",
-      startTime: new Date(),
-      qoe: 0
-    };
-  }
-
-  async componentDidMount() {
-    const { viewing } = this;
-    await viewing.init();
-    this.setState({
-      title: await viewing.title,
-      location: await viewing.location,
-      thumbnail: await viewing.thumbnail,
-      startTime: await viewing.startTime,
-      qoe: await viewing.qoe,
-      quality: await viewing.quality
-    });
-    const { regionalAverageQoE } = this.props;
-    const region = (await viewing.region) || {};
-    this.setState({
-      region,
-      regionalAverageQoE: await regionalAverageQoE.at(region)
-    });
-    const { hourlyAverageQoE } = this.props;
-    const startTime = await viewing.startTime;
-    const hour = startTime.getHours();
-    this.setState({
-      hour,
-      hourlyAverageQoE: await hourlyAverageQoE.at(hour)
-    });
-  }
-
-  render() {
-    const {
-      id,
-      title,
-      location,
-      thumbnail,
-      startTime,
-      qoe,
-      quality,
-      region,
-      regionalAverageQoE,
-      hour,
-      hourlyAverageQoE
-    } = this.state;
-
-    return (
-      <div className={style.modalMain}>
-        <div className={style.header}>
-          <a href={location} target="_blank" rel="noopener noreferrer">
-            <img
-              className={style.modalThumbnail}
-              src={thumbnail}
-              alt={title}
-              onError={e => {
-                e.target.src = NoImage;
-              }}
-            />
-          </a>
-          <div className={style.movieInfo}>
-            <span>{urlToVideoPlatform(location).name}</span>
-            <span>{toTimeString(startTime)}</span>
-          </div>
-        </div>
-        <div className={`${style.title} ${style.modalTitle}`}>{title}</div>
-        <VideoQuality {...quality} />
-        <Box mt={2} px={1}>
-          <QoEValueGraphList
-            value={qoe}
-            region={region}
-            regionalAverage={regionalAverageQoE}
-            hour={hour}
-            hourlyAverage={hourlyAverageQoE}
-            isDetail
+  return (
+    <div className={style.modalMain}>
+      <div className={style.header}>
+        <a href={location} target="_blank" rel="noopener noreferrer">
+          <VideoThumbnail
+            className={style.modalThumbnail}
+            title={title}
+            thumbnail={thumbnail}
           />
-        </Box>
-        <Button
-          color="default"
-          fullWidth
-          onClick={() => {
-            DataErase.add(id);
-            // FIXME: ViewingListをrender()しないと表示が変わらない
-            AppData.update(AppDataActions.ViewingList, state => state);
-            AppData.update(AppDataActions.Modal, null);
-          }}
-        >
-          <Typography variant="button" color="textSecondary">
-            この計測結果を削除する
-          </Typography>
-        </Button>
+        </a>
+        <div className={style.movieInfo}>
+          <span>{urlToVideoPlatform(location).name}</span>
+          <span>{toTimeString(startTime)}</span>
+        </div>
       </div>
-    );
-  }
-}
-
+      <div className={`${style.title} ${style.modalTitle}`}>{title}</div>
+      <VideoQuality {...quality} />
+      <Box mt={2} px={1}>
+        <QoEValueGraphList
+          value={qoe}
+          region={region}
+          regionalAverage={regionalAverage}
+          hour={startTime.getHours()}
+          hourlyAverage={hourlyAverage}
+          isDetail
+        />
+      </Box>
+      <Button
+        color="default"
+        fullWidth
+        onClick={() => {
+          DataErase.add(id);
+          // FIXME: ViewingListをrender()しないと表示が変わらない
+          AppData.update(AppDataActions.ViewingList, state => state);
+          AppData.update(AppDataActions.Modal, null);
+        }}
+      >
+        <Typography variant="button" color="textSecondary">
+          この計測結果を削除する
+        </Typography>
+      </Button>
+    </div>
+  );
+};
 ViewingDetail.propTypes = {
   sessionId: PropTypes.string.isRequired,
   videoId: PropTypes.string.isRequired,
   regionalAverageQoE: PropTypes.instanceOf(RegionalAverageQoE).isRequired,
   hourlyAverageQoE: PropTypes.instanceOf(HourlyAverageQoE).isRequired
 };
-export default ViewingDetail;
+export default React.memo(ViewingDetail);
