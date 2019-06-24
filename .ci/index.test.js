6e8cb7ef06a810c7b228cffb5261019a2d2e51e5
Kohei Watanabe <kou029w@gmail.com>
2019-06-24 10:46:46 +0900

    コードの整理

--- .ci/index.test.js
+++ .ci/index.test.js
@@ -22,16 +22,12 @@ afterEach(async () => {
 
 test(`利用規約とプライバシーポリシーに同意後、logView (${
   logView.pathname
-})が表示`, async () => {
-  expect(new URL(extensionPage.url()).pathname).toBe(logView.pathname);
+})が表示`, () => {
+  expect(new URL(page.url()).pathname).toBe(logView.pathname);
 });
 test("YouTube動画に埋め込み", async () => {
   const demoVideo = "https://www.youtube.com/watch?v=mY6sChi65oU";
-  const player = "#ytd-player";
   const videomark = "#__videomark_ui";
   await page.goto(demoVideo);
-  await page.waitFor(player);
-  await page.bringToFront();
-  await page.click(player);
-  await page.waitFor(videomark, { timeout: 2e3 });
+  await page.waitFor(videomark);
 });
