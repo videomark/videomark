45f18e7b5c146de3f38210b3464eb1504a59e8c2
Kohei Watanabe <kou029w@gmail.com>
2019-07-03 16:52:12 +0900

    Object.fromEntries対応

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import fromEntries from "object.fromentries";
 import App from "./App";
 import StatsSummary from "./StatsSummary";
 import * as serviceWorker from "./serviceWorker";
 import ChromeExtensionWrapper from "./js/utils/ChromeExtensionWrapper";
+
+// FIXME: for chrome version < 73
+Object.fromEntries = fromEntries;
 
 if (!ChromeExtensionWrapper.canUseVideoMarkApi()) {
   window.location.pathname = "unsupported.html";
