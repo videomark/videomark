e1ff5c7da5b7b6782ab7072cd904f200cd74925e
Kohei Watanabe <kou029w@gmail.com>
2019-07-12 17:12:02 +0900

    新しいデータ構造への対応

--- src/js/modules/Storage.js
+++ src/js/modules/Storage.js
@@ -8,13 +8,42 @@ export class Storage {
       session_id: this.sessionId,
       video_id: this.videoId
     };
+    this.initialized = false;
   }
 
   get viewingId() {
     return `${this.sessionId}_${this.videoId}`;
   }
 
+  async init() {
+    if (Config.is_mobile()) {
+      const { index } = await new Promise(resolve =>
+        window.sodium.storage.local.get("index", resolve)
+      );
+      if (!Array.isArray(index)) return;
+      await new Promise(resolve =>
+        window.sodium.storage.local.set(
+          { index: [...index, this.viewingId] },
+          resolve
+        )
+      );
+    } else {
+      window.postMessage(
+        {
+          type: "FROM_SODIUM_JS",
+          method: "init",
+          id: this.viewingId
+        },
+        "*"
+      );
+    }
+  }
+
   async save(attributes) {
+    if (!this.initialized) {
+      await this.init();
+      this.initialized = true;
+    }
     Object.assign(this.cache, attributes);
     if (Config.is_mobile()) {
       window.sodium.storage.local.set({ [this.viewingId]: this.cache });
