450b51c63deb143669363e36a6f079706b55b3e2
Kohei Watanabe <kou029w@gmail.com>
2019-07-25 13:44:55 +0900

    storage へのアクセスは他のプロセスをブロックするので開始前に一定時間待つ

--- src/Rollback.jsx
+++ src/Rollback.jsx
@@ -4,6 +4,7 @@ import Button from "@material-ui/core/Button";
 import SimplePage from "./js/components/SimplePage";
 import ErrorSnackbar from "./js/components/ErrorSnackbar";
 import { VERSION, rollback } from "./js/utils/ChromeExtensionWrapper";
+import waitForContentRendering from "./js/utils/waitForContentRendering";
 
 export default withRouter(() => {
   const [error, setError] = useState(null);
@@ -12,7 +13,11 @@ export default withRouter(() => {
   const main = async () => {
     setError(null);
     try {
-      if (doing) await rollback();
+      if (doing) {
+        // FIXME: storage へのアクセスは他のプロセスをブロックするので開始前に一定時間待つ
+        await waitForContentRendering();
+        await rollback();
+      }
     } catch (e) {
       setError(e);
     }
