b96d753599a93371483f396ad7b83bb2de7cfa6d
Kohei Watanabe <kou029w@gmail.com>
2019-08-02 13:31:54 +0900

    node v10 fromEntries無いケースに対応

--- .ci/index.js
+++ .ci/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const pages = Object.fromEntries(
-  require("glob")
-    .sync("./pages/*.js")
-    .map(pageFile => [path.basename(pageFile, ".js"), require(pageFile)])
-);
+const pages = {};
+require("glob")
+  .sync("./pages/*.js")
+  .map(pageFile => [path.basename(pageFile, ".js"), require(pageFile)])
+  .forEach(([key, value]) => Object.assign(pages, { [key]: value }));
 module.exports = { pages };
