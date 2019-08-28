2f54888d1cced6b3c01046c4421ed1cefe2fd654
Yuuhei Tazaki <taza@acutus.co.jp>
2019-08-28 19:38:24 +0900

    height frame rate

--- src/js/modules/YouTubeTypeHandler.js
+++ src/js/modules/YouTubeTypeHandler.js
@@ -589,7 +589,8 @@ class YouTubeTypeHandler {
             // eslint-disable-next-line camelcase
             const { quality_label, itag, type, size } = YouTubeTypeHandler.get_playable_video_format_list()
                 .find(e => Number.parseInt(e.bitrate, 10) === bitrate);
-            const quality = YouTubeTypeHandler.qualityLabelTable[quality_label.replace(/[^0-9^\\.]/g, "")];
+            // const quality = YouTubeTypeHandler.qualityLabelTable[quality_label.replace(/[^0-9^\\.]/g, "")];
+            const quality = YouTubeTypeHandler.qualityLabelTable[/\d+/.exec(quality_label)[0]];
             // eslint-disable-next-line camelcase, no-console
             console.log(`VIDEOMARK: Playback quality [quality:${quality_label}(${quality}), bitrate:${bitrate}, itag:${itag}, type:${type}, size:${size}]`);
             this.player.setPlaybackQualityRange(quality, quality);
