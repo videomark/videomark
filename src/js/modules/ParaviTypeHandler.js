6f77d3983bae35a5cbf1db6077ce8313ba6cd4b3
Yuuhei Tazaki <taza@acutus.co.jp>
2019-09-24 14:38:53 +0900

    eslit comment

--- src/js/modules/ParaviTypeHandler.js
+++ src/js/modules/ParaviTypeHandler.js
@@ -1,23 +1,27 @@
 export default class ParaviTypeHandler {
 
+    // eslint-disable-next-line camelcase
     static get_duration() {
         const duration = videojs.getAllPlayers()[0].duration();
 
         return !duration || Number.isNaN(duration) ? -1 : duration;
     }
 
+    // eslint-disable-next-line camelcase
     static get_video_width() {
         return videojs.getAllPlayers()[0].dash.shakaPlayer.getStats().width;
     }
 
+    // eslint-disable-next-line camelcase
     static get_video_height() {
         return videojs.getAllPlayers()[0].dash.shakaPlayer.getStats().height;
     }
 
+    // eslint-disable-next-line camelcase
     static get_bitrate() {
         const stat = videojs.getAllPlayers()[0].dash.shakaPlayer.getStats();
-        const variant_tracks = videojs.getAllPlayers()[0].dash.shakaPlayer.getVariantTracks();
-        if (!stat || !variant_tracks)
+        const vt = videojs.getAllPlayers()[0].dash.shakaPlayer.getVariantTracks();
+        if (!stat || !vt)
             return null;
 
         const audio = stat.switchHistory.filter(e => e.type === 'audio');
@@ -25,7 +29,7 @@ export default class ParaviTypeHandler {
         if (!audio || audio.length === 0 || !video || video.length === 0)
             return null;
 
-        const variant = variant_tracks
+        const variant = vt
             .find(e => e.audioId === audio[audio.length - 1].id &&
                 e.videoId === video[video.length - 1].id);
         if (!variant)
@@ -34,23 +38,44 @@ export default class ParaviTypeHandler {
         return variant.videoBandwidth;
     }
 
+    // eslint-disable-next-line camelcase
     static get_receive_buffer() {
         return videojs.getAllPlayers()[0].bufferedEnd();
     }
 
+    // eslint-disable-next-line camelcase
     static get_framerate() {
         return -1;
     }
 
+    // eslint-disable-next-line camelcase
     static get_segment_domain() {
         return document.domain;
     }
 
-    // eslint-disable-next-line no-unused-vars
+    // eslint-disable-next-line camelcase, no-unused-vars
     static get_current_time(video) {    // TVerのインターフェースと合わせる
         return videojs.getAllPlayers()[0].currentTime();
     }
 
+    // eslint-disable-next-line camelcase
+    static get_play_list_info() {
+        /* TODO
+        const formats = videojs.getAllPlayers()[0].dash.shakaPlayer.getVariantTracks();
+        return formats
+            .map(e => ({
+                representationId: e.id,
+                bps: e.videoBandwidth,
+                videoWidth: e.width,
+                videoHeight: e.height,
+                fps: e.frameRate,
+                chunkDuration: YouTubeTypeHandler.DEFAULT_SEGMENT_DURATION,
+                serverIp: new URL(e.url).host
+            }))
+        */
+    }
+
+    // eslint-disable-next-line camelcase
     static is_paravi_type() {
         try {
             if (videojs &&
