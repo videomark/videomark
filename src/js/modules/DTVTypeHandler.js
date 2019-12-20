83c1b142c95d6f8df62d266dc8c93d4b1a40abc4
Yuuhei Tazaki <taza@acutus.co.jp>
2019-12-20 15:28:35 +0900

    dTV用のハンドラを追加

---
+++ src/js/modules/DTVTypeHandler.js
@@ -0,0 +1,44 @@
+
+import GeneralTypeHandler from './GeneralTypeHandler';
+
+export default class DTVTypeHandler extends GeneralTypeHandler {
+
+    // eslint-disable-next-line camelcase, class-methods-use-this
+    get_video_title() {
+
+        try {
+
+            return document
+                .querySelector(".titleDetailHeading_title")
+                .textContent
+        } catch (e) {
+
+            return "";
+        }
+    }
+
+    // eslint-disable-next-line camelcase, class-methods-use-this
+    get_video_thumbnail() {
+
+        try {
+
+            return document
+                .querySelector(".is-active .libertyBtn_image img")
+                .src
+        } catch (e) {
+
+            return "";
+        }
+
+    }
+
+    // eslint-disable-next-line camelcase
+    is_main_video(video) {
+        return video === this.elm
+    }
+
+    // eslint-disable-next-line camelcase, no-unused-vars, class-methods-use-this
+    is_cm(video) {
+        return false;
+    }
+}
