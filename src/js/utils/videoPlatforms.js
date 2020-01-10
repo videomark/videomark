553f1e1bb4d0e18e4cc40c9b87dcfc3a4d32c502
Kohei Watanabe <kou029w@gmail.com>
2020-01-10 16:22:45 +0900

    ホスト名の正規化

--- src/js/utils/videoPlatforms.js
+++ src/js/utils/videoPlatforms.js
@@ -4,41 +4,41 @@ export default [
     name: "YouTube",
     url: "https://www.youtube.com/",
     brandcolor: "#cc0000",
-    host: /youtube\.com$/
+    host: /(^|\.)youtube\.com$/
   },
   {
     id: "paravi",
     name: "Paravi",
     url: "https://www.paravi.jp/",
     brandcolor: "#004f99",
-    host: /paravi\.jp$/
+    host: /(^|\.)paravi\.jp$/
   },
   {
     id: "tver",
     name: "TVer",
     url: "https://tver.jp/",
     brandcolor: "#40b8dc",
-    host: /tver\.jp$/
+    host: /(^|\.)tver\.jp$/
   },
   {
     id: "fod",
     name: "FOD",
     url: "https://fod.fujitv.co.jp/",
     brandcolor: "#800000",
-    host: /fod\.fujitv\.co\.jp$/
+    host: /(^|\.)fod\.fujitv\.co\.jp$/
   },
   {
     id: "nicovideo",
     name: "niconico",
     url: "https://nicovideo.jp/",
     brandcolor: "#333333",
-    host: /^(www|live\d)\.nicovideo\.jp$/
+    host: /(^|\.)nicovideo\.jp$/
   },
   {
     id: "nhk-ondemand",
     name: "NHKオンデマンド",
     url: "https://www.nhk-ondemand.jp/",
     brandcolor: "#f18b02",
-    host: /^www\.nhk-ondemand\.jp$/
+    host: /(^|\.)nhk-ondemand\.jp$/
   }
 ];
