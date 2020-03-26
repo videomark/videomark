f0eb80dc5d29726fd85130f139eae34551a3b9ea
hATrayflood <h.rayflood@gmail.com>
2020-03-26 18:43:04 +0900

    ピーク時のビットレート自動制御機能

--- src/js/modules/SessionData.js
+++ src/js/modules/SessionData.js
@@ -7,18 +7,24 @@ import Config from "./Config";
 import ResourceTiming from "./ResourceTiming";
 import VideoData from "./VideoData";
 import { useStorage } from "./Storage";
-import { saveTransferSize } from "./StatStorage";
+import { saveTransferSize, loadPeakTimeLimit } from "./StatStorage";
 import { version } from "../../../package.json";
 
-function set_max_bitrate(new_video) {
+async function set_max_bitrate(new_video) {
   const bitrate_control = Config.get_bitrate_control();
   const quota_bitrate = Config.get_quota_bitrate();
   let bitrate;
   if (bitrate_control && quota_bitrate) bitrate = Math.min(bitrate_control, quota_bitrate);
   else bitrate = bitrate_control || quota_bitrate;
-  const resolution = Config.get_resolution_control();
+  let resolution = Config.get_resolution_control();
 
-  if (bitrate_control || quota_bitrate || resolution) new_video.set_max_bitrate(bitrate, resolution);
+  const peak_time_limit = await loadPeakTimeLimit();
+  if (peak_time_limit) {
+    bitrate    = Math.min(bitrate,    peak_time_limit.bitrate);
+    resolution = Math.min(resolution, peak_time_limit.resolution);
+  }
+
+  if (bitrate_control || quota_bitrate || resolution || peak_time_limit) new_video.set_max_bitrate(bitrate, resolution);
 }
 
 class MainVideoChangeException extends Error {
--- src/js/modules/StatStorage.js
+++ src/js/modules/StatStorage.js
@@ -24,4 +24,45 @@ export async function saveTransferSize(transfer_diff) {
   transfer_size[month] = size;
   storage.set({ transfer_size });
 };
-export default saveTransferSize;
+
+export async function savePeakTimeLimit(new_peak_time_limit) {
+  if (!new_peak_time_limit) return;
+
+  const peak_time_limit = Object.assign({}, new_peak_time_limit);
+  const now = new Date();
+  peak_time_limit.last_updated = now.getTime();
+
+  if (!Config.is_mobile()) {
+    window.postMessage({
+      type: "FROM_SODIUM_JS",
+      method: "save_peak_time_limit",
+      peak_time_limit
+    }, "*");
+    return;
+  }
+
+  const storage = window.sodium.storage.local;
+  storage.set({ peak_time_limit });
+};
+
+export async function loadPeakTimeLimit() {
+  if (!Config.get_settings().peak_time_limit_enabled) return undefined;
+
+  const now = new Date();
+  let peak_time_limit = Config.get_peak_time_limit();
+  // 最終更新から1日以上経過で定義更新
+  const expired = (peak_time_limit.last_updated || 0) + 24 * 60 * 60 * 1000;
+
+  if (now.getTime() > expired) {
+    const ret = await fetch(Config.get_peak_time_limit_url());
+    if (ret.ok) {
+      peak_time_limit = await ret.json();
+      savePeakTimeLimit(peak_time_limit);
+    }
+  }
+
+  const limit_list = (peak_time_limit[now.getTimezoneOffset()] || peak_time_limit.default)[now.getDay()];
+  const formatter = new Intl.NumberFormat("en-US", {minimumIntegerDigits: 2});
+  const time = `${formatter.format(now.getHours())}:${formatter.format(now.getMinutes())}`;
+  return limit_list.find(limit => limit.begin <= time && limit.end >= time);
+};
