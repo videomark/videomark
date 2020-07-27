75a2cf5777523d006e874a531abf8f2564d67b61
Kohei Watanabe <kou029w@gmail.com>
2020-07-27 12:26:07 +0900

    GitHub Actions runner ホストが Paravi 視聴に対応していない地域なのでスキップ

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -44,7 +44,8 @@ test("YouTube動画に埋め込み", async () => {
   await page.waitFor(videomark);
 }, 90e3);
 
-test.each(["youtube", "paravi"])(
+// FIXME: GitHub Actions runner ホストが Paravi 視聴に対応していない地域なのでスキップ
+test.each(["youtube" /*, "paravi"*/])(
   "%s: 動画に埋め込み後、しばらく経つとQoE値が得られる",
   async (platform) => {
     const videomark = "#__videomark_ui";
