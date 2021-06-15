ed2122a461d9ff87f372f30175ee8b635a818c45
Kohei Yoshino <kohei.yoshino@gmail.com>
2021-06-15 01:10:08 -0400

    計測結果画面のダークモード対応
    Fix https://github.com/webdino/sodium/issues/299

--- packages/videomark-log-view/src/js/components/ThemeProvider.jsx
+++ packages/videomark-log-view/src/js/components/ThemeProvider.jsx
@@ -3,20 +3,26 @@ import PropTypes from "prop-types";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: { main: "#1c2d69" },
-    secondary: { main: "#d1101b" },
-  },
-});
+const ThemeProvider = ({ children }) => {
+  const theme = createMuiTheme({
+    palette: {
+      // This has to be in a component because of underlying `window.matchMedia()`
+      type: useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light',
+      primary: { main: "#1c2d69" },
+      secondary: { main: "#d1101b" },
+    },
+  });
 
-const ThemeProvider = ({ children }) => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    {children}
-  </MuiThemeProvider>
-);
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
+};
+
 ThemeProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
--- packages/videomark-log-view/src/js/components/Welcome/index.jsx
+++ packages/videomark-log-view/src/js/components/Welcome/index.jsx
@@ -12,6 +12,7 @@ import { ReactComponent as Usage } from "./usage.svg";
 import { isWeb } from "../../utils/Utils";
 import videoPlatforms from "../../utils/videoPlatforms";
 import dino from "../../../images/dino.png";
+import { useTheme } from "@material-ui/core/styles";
 
 const ExperimentalSnackbar = () => {
   const [open, setOpen] = useState(true);
@@ -29,51 +30,59 @@ const ImportButton = () => (
   </Button>
 );
 
-export default () => (
-  <>
-    {isWeb() ? <ExperimentalSnackbar /> : null}
-    <Box position="fixed" top={48} right={14}>
-      <Usage />
-    </Box>
-    <Box paddingTop={8}>
-      <Grid container justify="space-between">
-        <Grid item sm={6}>
-          <Box paddingTop={2} paddingBottom={2}>
-            <Typography component="h1" variant="h4">
-              Web VideoMark
+export default () => {
+  const { palette } = useTheme();
+
+  const usageStyle = {
+    filter: palette.type === 'dark' ? 'invert(1)' : 'invert(0.25)',
+  };
+
+  return (
+    <>
+      {isWeb() ? <ExperimentalSnackbar /> : null}
+      <Box position="fixed" top={48} right={14}>
+        <Usage style={usageStyle} />
+      </Box>
+      <Box paddingTop={8}>
+        <Grid container justify="space-between">
+          <Grid item sm={6}>
+            <Box paddingTop={2} paddingBottom={2}>
+              <Typography component="h1" variant="h4">
+                Web VideoMark
+              </Typography>
+              <Typography variant="subtitle1">
+                ネットワークは実サービス利用時の品質で評価する時代へ
+              </Typography>
+              {isWeb() ? <ImportButton /> : null}
+            </Box>
+            <Typography component="h2" variant="h6">
+              計測可能な動画配信サービス
             </Typography>
-            <Typography variant="subtitle1">
-              ネットワークは実サービス利用時の品質で評価する時代へ
+            <Box fontSize="body1.fontSize">
+              <ul>
+                {videoPlatforms.map(({ id, name, url, experimental }) =>
+                  experimental ? null : (
+                    <li key={id}>
+                      <Link href={url} color="secondary">{name}</Link>
+                    </li>
+                  )
+                )}
+              </ul>
+            </Box>
+            <Typography>
+              調査対象となる動画配信サービスでいつも通り動画をお楽しみください。
             </Typography>
-            {isWeb() ? <ImportButton /> : null}
-          </Box>
-          <Typography component="h2" variant="h6">
-            計測可能な動画配信サービス
-          </Typography>
-          <Box fontSize="body1.fontSize">
-            <ul>
-              {videoPlatforms.map(({ id, name, url, experimental }) =>
-                experimental ? null : (
-                  <li key={id}>
-                    <Link href={url}>{name}</Link>
-                  </li>
-                )
-              )}
-            </ul>
-          </Box>
-          <Typography>
-            調査対象となる動画配信サービスでいつも通り動画をお楽しみください。
-          </Typography>
-          <Typography>
-            動画視聴中に自動的に再生品質やネットワーク品質などの視聴情報を記録します。
-          </Typography>
+            <Typography>
+              動画視聴中に自動的に再生品質やネットワーク品質などの視聴情報を記録します。
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Box paddingTop={4} textAlign="center">
+              <img src={dino} alt="dinoくん" />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Box paddingTop={4} textAlign="center">
-            <img src={dino} alt="dinoくん" />
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  </>
-);
+      </Box>
+    </>
+  );
+};
