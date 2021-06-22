fb6988d96c91b001e0e878dcd804c7394c83b084
Kohei Yoshino <kohei.yoshino@gmail.com>
2021-06-21 21:34:54 -0400

    ボタンとリンクの文字色を修正

--- packages/videomark-log-view/src/js/components/ThemeProvider.jsx
+++ packages/videomark-log-view/src/js/components/ThemeProvider.jsx
@@ -4,15 +4,27 @@ import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { indigo, red } from '@material-ui/core/colors';
 
 const ThemeProvider = ({ children }) => {
+  const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
   const theme = createMuiTheme({
     palette: {
       // This has to be in a component because of underlying `window.matchMedia()`
-      type: useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light',
-      primary: { main: "#1c2d69" },
-      secondary: { main: "#d1101b" },
+      type: isDarkMode ? "dark" : "light",
+      primary: {
+        main: indigo[isDarkMode ? 'A100' : 700],
+      },
+      secondary: {
+        main: red[isDarkMode ? 'A100' : 700],
+      },
     },
+    typography: {
+      button: {
+        fontWeight: 'bold',
+      }
+    }
   });
 
   return (
