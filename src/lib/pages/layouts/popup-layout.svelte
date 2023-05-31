bff3990c535da2bd55752de0c603a3d1c65a244b
Kohei Yoshino <kohei.yoshino@gmail.com>
2023-05-31 19:46:37 -0400

    ツールバードロップダウンメニューの改善

--- src/lib/pages/layouts/popup-layout.svelte
+++ src/lib/pages/layouts/popup-layout.svelte
@@ -1,37 +1,62 @@
 <script>
+  import { Button, Icon } from '@sveltia/ui';
   import { onMount } from 'svelte';
+  import { _ } from 'svelte-i18n';
+  import { openTab } from '$lib/services/navigation';
   import Wordmark from '$lib/pages/common/wordmark.svelte';
 
-  // ポップアップのサイズを調整
+  // Adjust the popup size
   onMount(() => {
     Object.assign(document.body.style, {
       margin: 0,
       width: '400px',
-      'min-height': '40px',
-      'max-height': '800px',
+      height: '600px', // = max height in Chrome
     });
 
     Object.assign(document.querySelector('.app-shell').style, {
       position: 'relative',
-      overflow: 'auto',
+      overflow: 'hidden',
+      height: '100vh',
     });
   });
 </script>
 
-<div class="wrapper">
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  class="wrapper"
+  on:click|capture={(event) => {
+    if (event.target.matches('.close-popup')) {
+      // Close the popup (after waiting for a new tab being opened)
+      window.setTimeout(() => {
+        window.close();
+      }, 100);
+    }
+  }}
+>
   <header>
     <h1><Wordmark /></h1>
+    <Button class="ghost iconic close-popup" on:click={() => openTab('#/settings')}>
+      <Icon slot="start-icon" name="settings" label={$_('settings.title')} />
+    </Button>
   </header>
   <slot />
 </div>
 
 <style lang="scss">
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    height: 100%;
+  }
+
   header {
+    flex: none;
     display: flex;
     gap: 32px;
-    justify-content: flex-start;
+    justify-content: space-between;
     align-items: center;
-    padding: 16px;
+    padding: 12px;
   }
 
   h1 {
--- src/lib/pages/popup/popup-platform-list.svelte
+++ src/lib/pages/popup/popup-platform-list.svelte
@@ -5,20 +5,18 @@
   import { viewingHistory } from '$lib/services/history';
 </script>
 
-<div class="empty">
-  <div class="platforms">
-    <p>{$_('popup.compatiblePlatforms.description')}</p>
-    <PlatformList />
-  </div>
-  {#if $viewingHistory?.length}
-    <footer>
-      <Button on:click={() => window.location.replace('#/popup')}>
-        <Icon slot="start-icon" name="arrow_back" />
-        {$_('popup.backToHistory')}
-      </Button>
-    </footer>
-  {/if}
+<div class="platforms">
+  <p>{$_('popup.compatiblePlatforms.description')}</p>
+  <PlatformList />
 </div>
+{#if $viewingHistory?.length}
+  <footer>
+    <Button on:click={() => window.location.replace('#/popup')}>
+      <Icon slot="start-icon" name="arrow_back" />
+      {$_('popup.backToHistory')}
+    </Button>
+  </footer>
+{/if}
 
 <style lang="scss">
   .platforms {
@@ -34,6 +32,7 @@
     align-items: center;
     justify-content: space-between;
     gap: 32px;
+    margin: auto 0 0;
     padding: 0 16px;
     height: 40px;
     background-color: var(--secondary-background-color);
