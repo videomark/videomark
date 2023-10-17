7c96d6237c069206ddc62366746263f6d760c237
Kobayashi, Keigo <45305556+k1o0b1a9@users.noreply.github.com>
2023-10-17 16:22:56 +0900

    AbemaTVのビデオタイトル取得方法修正 (#246)

--- src/lib/content/sodium/modules/AbemaTVLiveTypeHandler.js
+++ src/lib/content/sodium/modules/AbemaTVLiveTypeHandler.js
@@ -227,7 +227,7 @@ export default class AbemaTVLiveTypeHandler extends GeneralTypeHandler {
 
   get_video_title() {
     try {
-      return document.querySelector('.com-tv-SlotHeading__title').textContent;
+      return document.querySelector('[class="com-m-TextIcon--dark"]')?.textContent;
     } catch (e) {
       return '';
     }
