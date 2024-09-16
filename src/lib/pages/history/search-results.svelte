5dc44c653f85894c90453c7c5167cb0fb3c6737d
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-09-16 18:27:01 -0400

    early work on infinite scroll

--- src/lib/pages/history/search-results.svelte
+++ src/lib/pages/history/search-results.svelte
@@ -2,16 +2,27 @@
   import NotFound from '$lib/pages/history/not-found.svelte';
   import { searchCriteria, searchResults } from '$lib/services/history';
   import { settings } from '$lib/services/settings';
+  import { derived } from 'svelte/store';
   import HistoryItem from './history-item.svelte';
+
+  const resultGroupSize = 15;
+  const observer = new IntersectionObserver();
+  let currentResultGroups = 1; // number of "groups" to be rendered
 
   $: showDuplicates = $settings.show_duplicate_videos;
   $: searchTerms = $searchCriteria.terms.trim();
+
+  // creating a smaller array from $searchResults
+  $: subSearchResults = derived(searchResults, ($searchResults) =>
+    $searchResults.slice(0, resultGroupSize),
+  );
+  $: subSearchDebug = console.log($subSearchResults);
 </script>
 
-{#if $searchResults.length}
+{#if $subSearchResults.length}
   <div class="grid">
-    {#each $searchResults as historyItem, index (historyItem.key)}
-      {@const firstIndex = $searchResults.findIndex(({ url }) => url === historyItem.url)}
+    {#each $subSearchResults as historyItem, index (historyItem.key)}
+      {@const firstIndex = $subSearchResults.findIndex(({ url }) => url === historyItem.url)}
       {#if showDuplicates || firstIndex === index}
         <HistoryItem {historyItem} />
       {/if}
