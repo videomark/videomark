3297afb028bfb79244c47a393d18b0c6b5342fcc
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-10-07 19:02:45 -0400

    Infinite scrolling on history page (#283)

--- src/lib/pages/history/search-results.svelte
+++ src/lib/pages/history/search-results.svelte
@@ -1,22 +1,39 @@
 <script>
   import NotFound from '$lib/pages/history/not-found.svelte';
   import { searchCriteria, searchResults } from '$lib/services/history';
+  import scroll from '$lib/services/infinite-scroll';
   import { settings } from '$lib/services/settings';
+  import { derived } from 'svelte/store';
   import HistoryItem from './history-item.svelte';
+
+  const resultGroupSize = 50; // constant number of results in each rendered group
+  let currentResultGroups = 1; // number of groups to be rendered
 
   $: showDuplicates = $settings.show_duplicate_videos;
   $: searchTerms = $searchCriteria.terms.trim();
+
+  // creating a smaller derived array from $searchResults
+  $: subSearchResults = derived(searchResults, ($searchResults) =>
+    $searchResults.slice(0, resultGroupSize * currentResultGroups),
+  );
+
+  const renderScroll = () => {
+    if (resultGroupSize * currentResultGroups < $searchResults.length) currentResultGroups++;
+  };
 </script>
 
-{#if $searchResults.length}
+{#if $subSearchResults.length}
   <div class="grid">
-    {#each $searchResults as historyItem, index (historyItem.key)}
-      {@const firstIndex = $searchResults.findIndex(({ url }) => url === historyItem.url)}
+    {#each $subSearchResults as historyItem, index (historyItem.key)}
+      {@const firstIndex = $subSearchResults.findIndex(({ url }) => url === historyItem.url)}
       {#if showDuplicates || firstIndex === index}
         <HistoryItem {historyItem} />
       {/if}
     {/each}
   </div>
+  {#if $subSearchResults.length < $searchResults.length}
+    <div class="loading" use:scroll on:infiniteScroll={renderScroll}></div>
+  {/if}
 {:else}
   <NotFound {searchTerms} />
 {/if}
@@ -26,5 +43,21 @@
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
     gap: 16px;
+  }
+
+  .loading {
+    margin: auto;
+    margin-top: 16px;
+    border: 4px dotted var(--sui-secondary-foreground-color);
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 4s linear infinite;
+  }
+
+  @keyframes spin {
+    to {
+      transform: rotate(1turn);
+    }
   }
 </style>
---
+++ src/lib/services/infinite-scroll.js
@@ -0,0 +1,23 @@
+/** @type {import('svelte/action').Action}  */
+export default function scroll(node) {
+  const options = { threshold: 1 }; // threshold: 1 = entire element must be visible to trigger
+
+  const callback = (entries) => {
+    entries.forEach((e) => {
+      if (e.isIntersecting) {
+        e.target.dispatchEvent(new CustomEvent('infiniteScroll'));
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  observer.observe(node);
+
+  return {
+    destroy() {
+      // disconnecting the observer when the component is no longer loaded
+      observer.disconnect();
+    },
+  };
+}
