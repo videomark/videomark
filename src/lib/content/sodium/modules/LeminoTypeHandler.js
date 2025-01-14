52d17d6d97190699027408dcb4da565648a60f2c
Kohei Yoshino <kohei.yoshino@gmail.com>
2025-01-13 19:25:47 -0500

    Lemino の計測問題を修正

--- src/lib/content/sodium/modules/LeminoTypeHandler.js
+++ src/lib/content/sodium/modules/LeminoTypeHandler.js
@@ -2,26 +2,25 @@ import GeneralTypeHandler from './GeneralTypeHandler';
 
 export default class LeminoTypeHandler extends GeneralTypeHandler {
   get_video_title() {
-    try {
-      return document.querySelector('.titleDetailHeading_title').textContent;
-    } catch (e) {
-      return '';
-    }
-  }
-
-  get_video_thumbnail() {
-    try {
-      return document.querySelector('.is-active .libertyBtn_image img').src;
-    } catch (e) {
-      return '';
-    }
+    return (
+      document
+        .querySelector('[class^="ContentsDetailPlayerIntro__TitleStyle"]')
+        ?.innerText.split(/\n+/)
+        .reverse()
+        .join(' – ') || null
+    );
   }
 
   is_main_video(video) {
-    return video === this.elm;
+    return (
+      video ===
+      /** @type {HTMLVideoElement} */ (document.querySelector('video:not([title="Advertisement"])'))
+    );
   }
 
   is_cm() {
-    return false;
+    return /** @type {HTMLVideoElement[]} */ ([
+      ...document.querySelectorAll('video[title="Advertisement"]'),
+    ]).some((video) => !!video.videoHeight);
   }
 }
